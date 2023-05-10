147afef6565825944c142aca75244adcfb10b069
bhandekunal16 <bhandekunal16@gmail.com>
2023-05-10 17:13:17 +0530

    created

---
+++ src/color.tsx
@@ -0,0 +1,10 @@
+export default {
+  PRIMARY: "#9400d3",
+  LIGHT: "#dcdcdc",
+  DANGER: "#ff0000",
+  WHITE: "#f8f8ff",
+  BORDER: "#fffaf0",
+  TRUE: "#008000",
+  DARK: "#000000",
+  ORANGE: "#FFA500",
+};
